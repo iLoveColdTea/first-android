d62529609eb28d4b6944a7555415a8777735c2bd
Tomas <tomkinas123@gmail.com>
2013-05-04 13:50:34 +0300

    implemented simple button

--- src/com/itomkinas/firstapp/MainActivity.java
+++ src/com/itomkinas/firstapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.itomkinas.firstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button button = (Button) findViewById(R.id.button1);
+		final TextView textView = (TextView) findViewById(R.id.textView1);
+		
+		button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	textView.setText("What's up?");
+            }
+        });
+		
 	}
 
 	@Override
